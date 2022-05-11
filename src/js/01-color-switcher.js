ca23a38abd10eb2f96c3bd049d47ac82c1434a4d
AChekov <4ekoff@gmail.com>
2022-05-11 16:53:51 +0300

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+// btnStart.setAttribute('disabled', 'disabled');
+
+function onBtnStartClick() {
+  btnStart.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled');
+  timerId = setInterval(intervalColorSwitcher, 500);
+}
+
+function onBtnStopClick() {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
+
+function intervalColorSwitcher() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

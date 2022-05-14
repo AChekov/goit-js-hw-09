b98639d6acf0cae9881b93f737f5453145190e90
AChekov <4ekoff@gmail.com>
2022-05-14 23:37:55 +0300

    add js timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,30 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btnRef = document.querySelector('button[data-start]');
+const dayRef = document.querySelector('[data-days]');
+const hourRef = document.querySelector('[data-hour]');
+const minRef = document.querySelector('[data-min]');
+const secRef = document.querySelector('[data-sec]');
+
+btnRef.addEventListener('click', onBtnClick);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}

20bef90083f8d5befc897fb5d678062197d337ec
AChekov <4ekoff@gmail.com>
2022-05-17 23:42:00 +0300

    update timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,13 +2,68 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const btnRef = document.querySelector('button[data-start]');
+const btnStartRef = document.querySelector('button[data-start]');
 const dayRef = document.querySelector('[data-days]');
-const hourRef = document.querySelector('[data-hour]');
-const minRef = document.querySelector('[data-min]');
-const secRef = document.querySelector('[data-sec]');
+const hourRef = document.querySelector('[data-hours]');
+const minRef = document.querySelector('[data-minutes]');
+const secRef = document.querySelector('[data-seconds]');
 
-btnRef.addEventListener('click', onBtnClick);
+let selectedDates = null;
+
+btnStartRef.setAttribute('disabled', 'disabled');
+
+btnStartRef.addEventListener('click', onBtnStartClick);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= options.defaultDate) {
+      Notify.failure('Please choose a date in the future');
+      return;
+    }
+    selectedDates = selectedDates[0];
+    btnStartRef.removeAttribute('disabled');
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+const timer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = selectedDates - currentTime;
+      const componentsTimer = convertMs(deltaTime);
+      updateTimerField(componentsTimer);
+    }, 1000);
+  },
+};
+
+function onBtnStartClick() {
+  timer.start();
+}
+
+function updateTimerField({ days, hours, minutes, seconds }) {
+  dayRef.textContent = `${days}`;
+  hourRef.textContent = `${hours}`;
+  minRef.textContent = `${minutes}`;
+  secRef.textContent = `${seconds}`;
+  console.log(`${days} : ${hours} : ${minutes} : ${seconds}`);
+}
+
+const addLeadingZero = value => String(value).padStart(2, '0');
+
+// console.log(addLeadingZero);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -17,14 +72,10 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }

9b7a2b710ca6409fad84eb8f1b0fc4aff6554688
AChekov <4ekoff@gmail.com>
2022-05-18 22:31:13 +0300

    update-timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,3 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-const 
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
